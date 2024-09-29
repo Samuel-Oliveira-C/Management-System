e5834a520bf74c9cbe829f028026045ed731f2e0
Samuel Oliveira <samuel.oleiv@gmail.com>
2024-09-29 10:28:11 -0300

    Criado a estrutura do back-end feito com o spring

---
+++ back-end/gerenciador-veiculos/src/main/java/com/projeto/gerenciador_veiculos/GerenciadorVeiculosApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.gerenciador_veiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorVeiculosApplication.class, args);
+	}
+
+}
---
+++ back-end/gerenciador-veiculos/src/test/java/com/projeto/gerenciador_veiculos/GerenciadorVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.gerenciador_veiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
